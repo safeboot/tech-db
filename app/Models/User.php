5409cfced5f4f0a3f8a08736c1943c9e2342729e
safeboot <mynamesafeboot@gmail.com>
2022-07-11 23:03:33 +0200

    Hello world.

---
+++ app/Models/User.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
+use Laravel\Sanctum\HasApiTokens;
+use Laravel\Scout\Searchable;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable, Searchable;
+
+    protected $primaryKey = 'user_uuid';
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+
+        'username',
+        'first_name',
+        'last_name',
+        'email',
+        'password',
+        'avatar',
+        'is_admin'
+
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+
+        'password',
+        'remember_token',
+
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+
+        'email_verified_at' => 'datetime',
+
+    ];
+
+    protected static function boot() {
+
+        parent::boot();
+
+        static::creating(function ($user) {
+
+            if(empty($user->user_uuid)) {
+
+                $user->user_uuid = Str::uuid();
+
+            }
+
+        });
+
+    }
+}
